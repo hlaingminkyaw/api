6e28de756658e5f364b9760ee4da442bbf555470
Hlaing Min Kyaw <hlaingminkyaw@agga.io>
2025-08-03 14:19:59 +0630

    Updated

--- crud/insert.js
+++ crud/insert.js
@@ -2,10 +2,13 @@ const { PrismaClient } = require("../generated/prisma");
 
 const prisma = new PrismaClient();
 async function main() {
-    const item= await prisma.todo.create({
-        data:{
-            name: "Something to do ",
-        }
+    const item= await prisma.todo.createMany({
+        data:[
+            {name: "Another thing to do"},
+            {name: "More thing to do "},
+            {name: "Completed thing to do "},
+
+        ]
     });
     console.log(item);
 }
